c2ee137ce81023c8048bece04404b8e85648824c
rogerfan <rfan@frbchi.org>
2013-12-13 15:22:26 -0600

    Refactored tests.

--- tests/test_pitchfx_dl.py
+++ tests/test_pitchfx_dl.py
@@ -1,112 +1,130 @@
 import os
 import shutil
 
-import nose
+from nose.tools import raises
 
 import pitchfx
 
 
-# Setup and Teardown Functions
-def c_folder_func(path):
-    def create_folder():
-        if not os.path.isdir(path):
-            os.makedirs(path)
-    return create_folder
+# Functions to create and delete folders
+def create_folder(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
 
-def d_folder_func(path):
-    def delete_folder():
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-    return delete_folder
+def delete_folder(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+
+# Tests
+class TestGetURL:
+    def test_basic(self):
+        url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
+               "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/linescore.xml")
+        test_texts = [
+            '''Copyright 2012 MLB Advanced Media, L.P.''',
+            '''home_name_abbrev="COL"''',
+            '''away_code="cha"''',
+            '''gameday_link="2012_03_11_chamlb_colmlb_1"''',
+            '''winning_pitcher first_name="Jamie" first="Jamie" id="119469"'''
+        ]
+        text = pitchfx._get_url(url)
+
+        for test_text in test_texts:
+            assert test_text in text
+
+    @raises(pitchfx.Error404)
+    def test_nonexistent_url(self):
+        url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
+               "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/linesgfcore.xml")
+        pitchfx._get_url(url)
 
 
-# Testing _create_folder()
-@nose.tools.with_setup(teardown=d_folder_func("test_folder"))
-def test_create_folder():
-    pitchfx._create_folder("test_folder")
-    assert os.path.isdir("test_folder")
+class TestConfirmRegularGame:
+    def setup(self):
+        self.urlstub = "http://gd2.mlb.com/components/game/mlb/year_2012"
 
-@nose.tools.with_setup(setup=c_folder_func("test_folder"),
-                       teardown=d_folder_func("test_folder"))
-def test_create_folder_already_Exists():
-    pitchfx._create_folder("test_folder")
-    assert os.path.isdir("test_folder")
+    def test_regular_game(self):
+        url = self.urlstub + "/month_06/day_10/gid_2012_06_10_nynmlb_nyamlb_1"
+        assert pitchfx._confirm_regular_game(url)
 
-# Testing _get_url()
-def test_get_url():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/linescore.xml")
-    test_texts = [
-        '''Copyright 2012 MLB Advanced Media, L.P.''',
-        '''home_name_abbrev="COL"''',
-        '''away_code="cha"''',
-        '''gameday_link="2012_03_11_chamlb_colmlb_1"''',
-        '''winning_pitcher first_name="Jamie" first="Jamie" id="119469"'''
-    ]
-    text = pitchfx._get_url(url)
+    def test_error_game(self):
+        url = self.urlstub + "month_06/day_10/gid_2012_06_10_tbamlb_flomlb_1"
+        assert not pitchfx._confirm_regular_game(url)
 
-    for test_text in test_texts:
-        assert test_text in text
+    def test_playoff_game(self):
+        url = self.urlstub + "month_10/day_06/gid_2012_10_06_oakmlb_detmlb_1/"
+        assert not pitchfx._confirm_regular_game(url)
+
+    def test_allstar_game(self):
+        url = self.urlstub + "month_07/day_10/gid_2012_07_10_nasmlb_aasmlb_1/"
+        assert not pitchfx._confirm_regular_game(url)
+
+    def test_springtrain_game(self):
+        url = self.urlstub + "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/"
+        assert not pitchfx._confirm_regular_game(url)
 
 
-@nose.tools.raises(pitchfx.Error404)
-def test_get_url_nonexistent():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/linesgfcore.xml")
-    pitchfx._get_url(url)
+class TestDLGameData:
+    def setup(self):
+        self.folder   = "test_folder/"
+        self.url_loc  = ("http://gd2.mlb.com/components/game/mlb/" +
+                         "year_2012/month_06/day_10/")
+        self.gamename = "gid_2012_06_10_nynmlb_nyamlb_1"
+
+        create_folder(self.folder)
+
+    def teardown(self):
+        delete_folder(self.folder)
+
+    def test_basic(self):
+
+        pitchfx._dl_game_data(self.url_loc, self.folder, self.gamename)
+
+        assert os.path.isdir(self.folder + self.gamename + "/batters")
+        assert os.path.isfile(self.folder + self.gamename + "/batters/121250.xml")
+        assert os.path.isfile(self.folder + self.gamename + "/batters/579799.xml")
+
+        assert os.path.isdir(self.folder + self.gamename + "/pitchers")
+        assert os.path.isfile(self.folder + self.gamename + "/pitchers/110683.xml")
+        assert os.path.isfile(self.folder + self.gamename + "/pitchers/579799.xml")
+
+        assert os.path.isfile(self.folder + self.gamename + "/boxscore.xml")
+        assert os.path.isfile(self.folder + self.gamename + "/game.xml")
+        assert os.path.isfile(self.folder + self.gamename + "/inning_all.xml")
+        assert os.path.isfile(self.folder + self.gamename + "/players.xml")
 
 
-# Testing _confirm_regular_game()
-def test_confirm_regular_game_pass():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_06/day_10/gid_2012_06_10_nynmlb_nyamlb_1")
-    assert pitchfx._confirm_regular_game(url)
+class TestDLPitchFXData:
+    def setup(self):
+        self.folder = "test_folder"
+        create_folder(self.folder)
+
+    def teardown(self):
+        delete_folder(self.folder)
+
+    @raises(ValueError)
+    def test_0dates(self):
+        pitchfx.dl_pitchfx_data((),
+            self.folder, date_list=False,
+            max_workers=50, timeout=10, sleep=1, retry=False)
+
+    @raises(ValueError)
+    def test_m2dates(self):
+        pitchfx.dl_pitchfx_data(("2012-09-20", "2012-09-20", "2012-09-21"),
+            self.folder, date_list=False,
+            max_workers=50, timeout=10, sleep=1, retry=False)
+
+    @raises(ValueError)
+    def test_bad_date_format1(self):
+        pitchfx.dl_pitchfx_data(("2012-09-20", "2012-15-20"),
+            self.folder, date_list=False,
+            max_workers=50, timeout=10, sleep=1, retry=False)
+
+    @raises(ValueError)
+    def test_bad_date_format2(self):
+        pitchfx.dl_pitchfx_data(("9/20/2012"),
+            self.folder, date_list=False,
+            max_workers=50, timeout=10, sleep=1, retry=False)
 
 
-def test_confirm_nonregular_game_fail():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_06/day_10/gid_2012_06_10_tbamlb_flomlb_1")
-    assert not pitchfx._confirm_regular_game(url)
-
-
-def test_confirm_playoff_game_fail():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_10/day_06/gid_2012_10_06_oakmlb_detmlb_1/")
-    assert not pitchfx._confirm_regular_game(url)
-
-
-def test_confirm_allstar_game_fail():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_07/day_10/gid_2012_07_10_nasmlb_aasmlb_1/")
-    assert not pitchfx._confirm_regular_game(url)
-
-
-def test_confirm_springtrain_game_fail():
-    url = ("http://gd2.mlb.com/components/game/mlb/year_2012/" +
-           "month_03/day_11/gid_2012_03_11_chamlb_colmlb_1/")
-    assert not pitchfx._confirm_regular_game(url)
-
-
-# Testing _dl_game_data()
-@nose.tools.with_setup(setup=c_folder_func("test_folder"),
-                       teardown=d_folder_func("test_folder"))
-def test_dl_game_data():
-    url_loc  = "http://gd2.mlb.com/components/game/mlb/year_2012/month_06/day_10/"
-    gamename = "gid_2012_06_10_nynmlb_nyamlb_1"
-    loc      = "test_folder"
-
-    pitchfx._dl_game_data(url_loc, loc, gamename, max_workers=5)
-
-    assert os.path.isdir(loc + "/" + gamename + "/batters")
-    assert os.path.isfile(loc + "/" + gamename + "/batters/121250.xml")
-    assert os.path.isfile(loc + "/" + gamename + "/batters/579799.xml")
-
-    assert os.path.isdir(loc + "/" + gamename + "/pitchers")
-    assert os.path.isfile(loc + "/" + gamename + "/pitchers/110683.xml")
-    assert os.path.isfile(loc + "/" + gamename + "/pitchers/579799.xml")
-
-    assert os.path.isfile(loc + "/" + gamename + "/boxscore.xml")
-    assert os.path.isfile(loc + "/" + gamename + "/game.xml")
-    assert os.path.isfile(loc + "/" + gamename + "/inning_all.xml")
-    assert os.path.isfile(loc + "/" + gamename + "/players.xml")
